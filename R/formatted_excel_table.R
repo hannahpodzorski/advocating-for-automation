a7860751b89aa4023d27811c8feedc67694c0cca
hannahpodzorski <hpodzorski@gmail.com>
2022-07-16 15:34:26 -0700

    Initial Commit

---
+++ R/formatted_excel_table.R
@@ -0,0 +1,129 @@
+# Example xlsx table
+# Data Source: https://data.ca.gov/dataset/ground-water-water-quality-results/resource/9e09ac40-b694-4ddc-acd2-2e7f7d10e231
+#             (Downloaded 2022-07-11)
+
+
+library(tidyverse)
+library(openxlsx)
+library(lubridate)
+
+# Load data ---------------
+# df <- read.csv("./data/gama_2022-07-01.csv")
+# 
+# d_BTEX <- df %>% filter(gm_chemical_name %in% c("Benzene", "Toluene", "Ethylbenzene", "Xylenes (total)")) %>%
+#   select(gm_well_id, gm_samp_collection_date, gm_chemical_name, gm_result, gm_result_units, gm_result_modifier, gm_reporting_limit)
+# 
+# write.csv(d_BTEX, "./data/gama_2022-07-01_BTEX.csv", row.names = F)
+# 
+# d_cations <- df %>% filter(gm_chemical_name %in% c("Calcium", "Chloride", "Fluoride", "Magnesium", "Potassium", "Sodium")) %>%
+#   select(gm_well_id, gm_samp_collection_date, gm_chemical_name, gm_result, gm_result_units, gm_result_modifier, gm_reporting_limit)
+# 
+# write.csv(d_cations, "./data/gama_2022-07-01_ions.csv", row.names = F)
+
+d <- read.csv("./data/gama_2022-07-01_ions.csv")
+
+d <- d %>%
+  mutate(date = ymd(gm_samp_collection_date),
+         analyte = factor(gm_chemical_name, 
+                          levels = c("Calcium", "Chloride", "Fluoride", "Magnesium", "Potassium", "Sodium"),
+                          ordered = T),
+         Units = gsub("mg/l", "mg/L", tolower(gm_result_units)),
+         detected = !gm_result_modifier %in% c("ND", "<"), # identifying detects
+         result = ifelse(detected == F, gm_reporting_limit, gm_result)) %>% # using reporting limit if ND
+  filter(!is.na(result)) 
+
+# Summary Data
+d_sum <- d %>%
+  group_by(gm_well_id, gm_chemical_name, Units) %>% 
+  summarise(n = n(),
+            detected = sum(detected),
+            min_date = min(date),
+            max_date = max(date),
+            min = min(result),
+            max = max(result),
+            mean = mean(result)) %>%
+  arrange(gm_well_id, gm_chemical_name) %>%
+  select(`Well ID` = gm_well_id, `Chemical Name` = gm_chemical_name, Units,
+         `Samples` = n, `First Date Sampled` = min_date, `Last Date Samples` = max_date, 
+         `Minium Sample` = min, `Mean Sample` = mean, `Maximum Sample` = max) %>%
+  filter(`Well ID` == "100832") %>% mutate(`Well ID` = "MW-1") %>%
+  mutate(across(contains("Sample"), ~round(.x, 2)))
+
+# openxlsx Formats
+col_style <- createStyle(
+  fontName = "Arial",
+  fontSize = 12,
+  fontColour = "black",
+  halign = "center",
+  valign = "center",
+  textDecoration = "bold",
+  fgFill = "#D9D9D9",
+  wrapText = T)
+
+d_style <- createStyle(
+  fontName = "Arial",
+  fontSize = 10,
+  fontColour = "black",
+  halign = "left",
+  valign = "center",
+  wrapText = T)
+
+d_cen_style <- createStyle(
+  fontName = "Arial",
+  fontSize = 10,
+  fontColour = "black",
+  halign = "center",
+  valign = "center",
+  wrapText = T)
+
+border_style_bottom_double <- createStyle(
+  border = "bottom",
+  borderStyle = "double")
+
+border_style_bottom <- createStyle(
+  border = "bottom",
+  borderStyle = "thin")
+
+border_style_top <- createStyle(
+  border = "top",
+  borderStyle = "thin")
+
+
+border_style_right <- createStyle(
+  border = "right",
+  borderStyle = "thin")
+border_style_left <- createStyle(
+  border = "left",
+  borderStyle = "thin")
+
+# Format
+wb <- createWorkbook()
+sheet <- "Summary Stats"
+addWorksheet(wb, sheet)
+showGridLines(wb, sheet, showGridLines = F)
+
+writeData(wb, sheet, x = d_sum, 
+          startCol = 2, startRow = 2, borders = c("surrounding"), borderStyle = getOption("openxlsx.borderStyle", "thin"))
+
+# Merge Cells
+mergeCells(wb, sheet, rows = 2:(dim(d_sum)[1] + 2), cols = 2)
+
+# Style
+addStyle(wb, sheet, col_style, rows = 2, cols = 2:(dim(d_sum)[2] + 1), gridExpand = T, stack = T)
+addStyle(wb, sheet, border_style_bottom_double, rows = 2, cols = 2:(dim(d_sum)[2] + 1), gridExpand = T, stack = T)
+addStyle(wb, sheet, border_style_right, rows = 2:(dim(d_sum)[1] + 2), cols = 2:(dim(d_sum)[2] + 1), gridExpand = T, stack = T)
+
+addStyle(wb, sheet, d_cen_style, rows = 3:(dim(d_sum)[1] + 2), cols = c(1, 4:(dim(d_sum)[2] + 1)), gridExpand = T, stack = T)
+addStyle(wb, sheet, d_style, rows = 3:(dim(d_sum)[1] + 2), cols = 3, gridExpand = T, stack = T)
+addStyle(wb, sheet, border_style_bottom, rows = 3:(dim(d_sum)[1] + 2), cols = 2:(dim(d_sum)[2] + 1), gridExpand = T, stack = T)
+addStyle(wb, sheet, border_style_top, rows = 2, cols = 2:(dim(d_sum)[2] + 1), gridExpand = T, stack = T)
+addStyle(wb, sheet, border_style_left, rows = 2:(dim(d_sum)[1] + 2), cols = 2, gridExpand = T, stack = T)
+
+# Final Touches
+# col_widths <- c()
+# 
+# setColWidths(wb, sheet, cols = 1:dim(d_sum)[1], widths = col_widths)
+
+pageSetup(wb, sheet, orientation = "landscape", fitToWidth = T)
+
+saveWorkbook(wb, "./R/Summary-Stats.xlsx", overwrite = T)
---
+++ R/reaction-energy.R
@@ -0,0 +1,110 @@
+# Activation Energy Plot
+
+library(tidyverse)
+
+gsi_colors <- c("blue" = "#1C6EAC", "dark-blue" = "#005782")
+# Parabolic functions
+para_1 <- function(x){
+  h = 500 # x-coordinate of vertex
+  k = 500 # y-coordinate of vertex
+  a = -0.0064 
+  
+  a*(x - h)^2 + k
+} 
+
+para_2 <- function(x) {
+  h = 500 # x-coordinate of vertex
+  k = 250 # y-coordinate of vertex
+  a = -0.0024 
+  
+  a*(x - h)^2 + k}
+
+para_3 <- function(x) {
+  h = 500 # x-coordinate of vertex
+  k = 250 # y-coordinate of vertex
+  a = -0.003622222 
+  
+  a*(x - h)^2 + k}
+
+# Creating data sets
+x <- 1:1200
+
+d1 <- data.frame(x = x,
+                y = c(rep.int(para_1(250), 249), 
+                      para_1(x[250:800]),
+                      rep.int(para_1(800), 400)))
+
+d2 <- data.frame(x = x,
+                 y = c(rep.int(NA, 249), 
+                       para_2(x[250:500]),
+                       para_3(x[501:800]),
+                       rep.int(NA, 400)))
+
+p1 <- ggplot() + 
+  # Add Parabolas
+  geom_path(data = d1, aes(x,y), size = 3) +
+  # Add Starting and Ending Points
+  geom_point(aes(x = 125, y = 200), size = 25, shape = 21, stroke = 2,
+             color = gsi_colors["dark-blue"], fill = gsi_colors["blue"]) +
+  geom_point(aes(x = 1000, y = 24), size = 25, shape = 21, stroke = 2,
+             color = gsi_colors["dark-blue"], fill = gsi_colors["blue"]) +
+  annotate("text", x = 125, y = para_1(250), 
+           label = "Current\nWorkflow", 
+           vjust = 1.25, hjust = 0.5, size = 8) +
+  annotate("text", x = 1000, y = para_1(800), 
+           label = "Automated\nWorkflow", 
+           vjust = 1.25, hjust = 0.5, size = 8) +
+  # Activation Energy Label
+  geom_segment(aes(x = 250, xend = 799, y = 500, yend = 500), linetype = "dotted", size = 1.5, color = "grey") +
+  geom_segment(aes(x = 250, xend = 799, y = 100, yend = 100), linetype = "dotted", size = 1.5, color = "grey") +
+  geom_segment(aes(x = 800, xend = 800, y = 100, yend = 500), 
+               arrow = arrow(length = unit(0.02, "npc"), ends = "both", type = "closed"), 
+               size = 1.5, color = "grey") +
+  annotate("text", x = 800, y = 300, label = "Activation Energy", size = 6, hjust = -0.1) +
+  # Axes
+  labs(x = "Pathway", y = "Total Energy") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(limit = c(-250, 500)) +
+  # Themeing
+  theme_classic(base_family = "sans", base_size = 28) +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.line = element_line(arrow = arrow(type='closed', length = unit(13,'pt')),
+                                 size = 2),
+        panel.background = element_rect(fill = "transparent", color = NA))
+
+
+p2 <- ggplot() + 
+  # Add Parabolas
+  geom_path(data = d1, aes(x,y), size = 3) +
+  geom_path(data = d2, aes(x,y), size = 2, color = "grey") + 
+  # Add Starting and Ending Points
+  geom_point(aes(x = 125, y = 200), size = 25, shape = 21, stroke = 2,
+             color = gsi_colors["dark-blue"], fill = gsi_colors["blue"]) +
+  geom_point(aes(x = 1000, y = 24), size = 25, shape = 21, stroke = 2,
+             color = gsi_colors["dark-blue"], fill = gsi_colors["blue"]) +
+  annotate("text", x = 125, y = para_1(250), 
+           label = "Current\nWorkflow", 
+           vjust = 1.25, hjust = 0.5, size = 8) +
+  annotate("text", x = 1000, y = para_1(800), 
+           label = "Automated\nWorkflow", 
+           vjust = 1.25, hjust = 0.5, size = 8) +
+  # Activation Energy Label
+  geom_segment(aes(x = 250, xend = 799, y = 250, yend = 250), linetype = "dotted", size = 1.5, color = "grey") +
+  geom_segment(aes(x = 250, xend = 799, y = 100, yend = 100), linetype = "dotted", size = 1.5, color = "grey") +
+  geom_segment(aes(x = 800, xend = 800, y = 100, yend = 250), 
+               arrow = arrow(length = unit(0.02, "npc"), ends = "both", type = "closed"), 
+               size = 1.5, color = "grey") +
+  annotate("text", x = 800, y = 175, label = "Activation Energy", size = 6, hjust = -0.1) +
+  # Axes
+  labs(x = "Pathway", y = "Total Energy") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(limit = c(-250, 500)) +
+  # Themeing
+  theme_classic(base_family = "sans", base_size = 28) +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.line = element_line(arrow = arrow(type='closed', length = unit(13,'pt')),
+                                 size = 2),
+        panel.background = element_rect(fill = "transparent", color = NA))
+
